4c9ff09cedca285b2ac479717eee64a2d16b9564
chrisbrasington <chrisbrasington@gmail.com>
2017-08-06 12:59:51 -0600

    suck it

---
+++ speedtestrandom.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+c="/usr/local/bin/speedtest-cli"
+## 0-5 minute random interval or 300 seconds
+r=$(( $RANDOM % 300 ));
+m=$(( r/60 ));
+s=$(( r-(m*60)  ));
+
+## sleep at random interval
+echo "Waiting for $m minutes, $s seconds."
+sleep $r
+
+## execute command
+echo "Running $c"
+eval $c
